616be72b48bd9a99123b6b151ea8ec6f811d1862
Tushar Jamadar <tusharjamadar2001@gmail.com>
2025-10-14 13:01:38 +0530

    removed wrong folders

--- product-service/src/main/java/com/tushar/ServiceRegistry/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tushar.ServiceRegistry.repository;
-
-import com.tushar.ServiceRegistry.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
