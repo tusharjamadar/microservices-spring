14c0e75cdfd83c0c7208b36fc3cd7b81bce83715
Tushar Jamadar <tusharjamadar2001@gmail.com>
2025-11-09 21:23:09 +0530

    cloudGateway boiler plate code added

---
+++ CloudGateway/src/test/java/com/tushar/CloudGateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.CloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
